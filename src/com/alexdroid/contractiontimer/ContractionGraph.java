af472e97d857c6d9bd3740751d4f02fb69618f99
Alex Murray <murray.alex@gmail.com>
2010-12-08 13:44:21 +1030

    Remove member reference to GraphView from Graph
    This is not referenced anywhere outside of onCreate() so no need to not
    scope it to just that method.

--- src/com/alexdroid/contractiontimer/ContractionGraph.java
+++ src/com/alexdroid/contractiontimer/ContractionGraph.java
@@ -4,16 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class ContractionGraph extends Activity {
-	private ContractionGraphView mGraphView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.contraction_graph);
 
-		mGraphView = (ContractionGraphView)findViewById(R.id.contraction_graph_view);
 		ContractionStore store = new ContractionStore(this);
-		mGraphView.setContractions(store.getAllContractions());
+		ContractionGraphView graphView = (ContractionGraphView)findViewById(R.id.contraction_graph_view);
+		graphView.setContractions(store.getAllContractions());
 		store.close();
 	}
 }
