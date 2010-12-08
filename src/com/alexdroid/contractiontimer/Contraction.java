64cf1d50ea26d99f49dc009a65b0d301c9dd94a6
Alex Murray <murray.alex@gmail.com>
2010-12-09 00:39:06 +1030

    Format time of contraction better and cleanup

--- src/com/alexdroid/contractiontimer/Contraction.java
+++ src/com/alexdroid/contractiontimer/Contraction.java
@@ -1,36 +1,38 @@
 package com.alexdroid.contractiontimer;
 
 public class Contraction {
-	private long id;
-	private long startMillis;
-	private long lengthMillis;
+	private long mId;
+	private long mStartMillis;
+	private long mLengthMillis;
 
 	public Contraction(long id, long startMillis, long lengthMillis)
 	{
-		this.id = id;
-		this.startMillis = startMillis;
-		this.lengthMillis = lengthMillis;
+		mId = id;
+		mStartMillis = startMillis;
+		mLengthMillis = lengthMillis;
 	}
 
 	public long getID()
 	{
-		return id;
+		return mId;
 	}
 
 	public long getStartMillis()
 	{
-		return startMillis;
+		return mStartMillis;
 	}
 
 	public long getLengthMillis()
 	{
-		return lengthMillis;
+		return mLengthMillis;
 	}
 
 	public String toString()
 	{
-		return android.text.format.DateFormat.format("MMM dd, yyyy h:mmaa", startMillis) + ": " +
-			android.text.format.DateUtils.formatElapsedTime(lengthMillis / 1000);
+		return android.text.format.DateUtils.getRelativeTimeSpanString(mStartMillis,
+				java.lang.System.currentTimeMillis(), 0) + ": " +
+
+			android.text.format.DateUtils.formatElapsedTime(mLengthMillis / 1000);
 	}
 }
 
