e151002629bc8464c127c04d4a8a42bc39cccacd
Alex Murray <murray.alex@gmail.com>
2010-12-08 21:18:49 +1030

    Ensure graph is a minimum size (240px for now)

--- src/com/alexdroid/contractiontimer/ContractionGraphView.java
+++ src/com/alexdroid/contractiontimer/ContractionGraphView.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 public class ContractionGraphView extends View {
 	private static final String TAG = "ContractionGraphView";
+	private static final int mMinWidth = 240;
 
 	private ShapeDrawable mDrawable;
 	private ArrayList<Contraction> mContractions;
@@ -29,10 +30,13 @@ public class ContractionGraphView extends View {
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		/* use super to handle this but then override with our required width */
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-		/* set our width depending on size */
-		int w = (int)((mMaxMillis - mMinMillis) * mResolution);
+		/* set our width depending on size - ensure we aren't smaller than
+		 * mMinWidth */
+		int w = Math.max(mMinWidth, (int)((mMaxMillis - mMinMillis) * mResolution));
+		/* use super's value of measured height */
 		int h = getMeasuredHeight();
 		Log.v(TAG, "onMeasure: w = " + w + " h = " + h);
 		setMeasuredDimension(w, h);
@@ -90,13 +94,15 @@ public class ContractionGraphView extends View {
 				mMaxMillis = maxMillis;
 			}
 		}
+		/* update resolution to ensure we aren't smaller than mMinWidth */
+		mResolution = Math.max(mResolution, (float)mMinWidth / (float)(mMaxMillis - mMinMillis));
 		Log.v(TAG, "setContractions: mMinMillis = " + mMinMillis + " mMaxMillis = " + mMaxMillis);
 		/* schedule relayout and hence redraw */
 		requestLayout();
 	}
 
 	public void setResolution(float resolution) {
-		mResolution = resolution;
+		mResolution = Math.max(resolution, (float)mMinWidth / (float)(mMaxMillis - mMinMillis));
 		requestLayout();
 	}
 
